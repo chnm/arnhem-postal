cfbcbaba575892507569c1fe3e034eb16c8710d7
Jason Heppler <jason.heppler@gmail.com>
2023-09-27 08:48:12 -0500

    Fixed error when no file was attached to an Object view

--- postcards/admin.py
+++ postcards/admin.py
@@ -1,20 +1,31 @@
 from django.contrib import admin
 from django.contrib.admin.widgets import AdminFileWidget
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
 from django.utils.html import format_html
 from import_export.admin import ImportExportMixin
 
-from .models import Archive, Collection, Image, Location, Object, Person, Postmark
+from .models import Language, Location, Object, Person, Postmark, Transcription
 from .resources import LocationResource, PersonResource, PostmarkResource
 
-admin.site.register(Archive)
-admin.site.register(Collection)
+# Rename our admin panel
+admin.site.site_header = "Arnhem Postal History Project"
+admin.site.site_title = "Arnhem Postal History Project"
+admin.site.index_title = "Arnhem Postal History Project"
+
+# Register models we haven't added custom adjustments to
+# admin.site.register(Archive)
+# admin.site.register(Collection)
+admin.site.register(Language)
 
 
+# Custom adjustments to our admin views
 class CustomAdminFileWidget(AdminFileWidget):
+    """This custom view allows us to create a preview image in the individual Object view"""
+
     def render(self, name, value, attrs=None, renderer=None):
-        result = []
-        if hasattr(value, "url"):
+        if value:
+            result = []
             result.append(
                 f"""<a href="{value.url}" target="_blank">
                       <img 
@@ -24,12 +35,15 @@ class CustomAdminFileWidget(AdminFileWidget):
                       />
                     </a>"""
             )
-        result.append(super().render(name, value, attrs, renderer))
-        return format_html("".join(result))
+            result.append(super().render(name, value, attrs, renderer))
+            return format_html("".join(result))
+        else:
+            return super().render(name, None, attrs=attrs, renderer=renderer)
 
 
 class CustomAdminMapWidget(AdminFileWidget):
-    """This provides an embedded map view showing the point on the lat/lon of the data using OSM"""
+    """This provides an embedded map view showing the point on the lat/lon of the data using OSM that we
+    display in the individual Location view"""
 
     def render(self, name, value, attrs=None, renderer=None):
         result = []
@@ -42,6 +56,8 @@ class CustomAdminMapWidget(AdminFileWidget):
 
 
 class PostmarkAdmin(ImportExportMixin, admin.ModelAdmin):
+    """We provide import/export abilities to the postmarks."""
+
     resource_class = PostmarkResource
     list_display = ("location", "date")
 
@@ -50,7 +66,10 @@ admin.site.register(Postmark, PostmarkAdmin)
 
 
 class LocationAdmin(ImportExportMixin, admin.ModelAdmin):
+    """We provide import/export abilities to the locations, as well as display the custom map widget."""
+
     resource_class = LocationResource
+    list_per_page = 25
     list_display = ("town_city", "province_state", "country", "map_preview")
     formfield_overrides = {models.FileField: {"widget": CustomAdminMapWidget}}
 
@@ -58,7 +77,16 @@ class LocationAdmin(ImportExportMixin, admin.ModelAdmin):
 admin.site.register(Location, LocationAdmin)
 
 
+class TranscriptionInline(admin.TabularInline):
+    """We provide the ability for transcriptions to happen as an in-line component to the Objects"""
+
+    model = Transcription
+
+
 class ObjectAdmin(ImportExportMixin, admin.ModelAdmin):
+    """We provide import/export abilities to the objects, as well as the inline transcriptions for
+    the objects."""
+
     list_display = (
         "date_of_correspondence",
         "sender_name",
@@ -67,6 +95,7 @@ class ObjectAdmin(ImportExportMixin, admin.ModelAdmin):
         "letter_enclosed",
         "image_canvas",
     )
+    inlines = [TranscriptionInline]
     search_fields = ["item_number", "sender_name", "addressee_name"]
     model = Object.item_id
     formfield_overrides = {models.FileField: {"widget": CustomAdminFileWidget}}
@@ -77,6 +106,8 @@ admin.site.register(Object, ObjectAdmin)
 
 
 class PersonAdmin(ImportExportMixin, admin.ModelAdmin):
+    """We provide the ability to import/export people."""
+
     list_display = (
         "last_name",
         "first_name",
@@ -90,25 +121,10 @@ class PersonAdmin(ImportExportMixin, admin.ModelAdmin):
 admin.site.register(Person, PersonAdmin)
 
 
-# Display images in the admin interface
-# https://stackoverflow.com/questions/10390244/how-to-display-images-in-django-admin
-class ImageInline(admin.ModelAdmin):
-    @admin.display(description="Image")
-    def image_tag(self, obj):
-        return format_html(
-            '<img src="{}" style="max-width:200px; max-height:200px"/>'.format(
-                obj.image.url
-            )
-        )
-
-    list_display = ["image_id", "image_tag", "image_caption"]
-
-
-admin.site.register(Image, ImageInline)
-
-
-# When looking at an individual Postcard, we want to see the Postmark and the Image.
-# We can do this by adding a PostmarkInline and ImageInline to the PostcardAdmin class.
 class PostmarkInline(admin.TabularInline):
+    """When looking at an individual Postcard, we want to see the Postmark and the Image.
+    We can do this by adding a PostmarkInline and ImageInline to the PostcardAdmin class.
+    """
+
     model = Postmark
     extra = 1
