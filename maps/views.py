398ff56421ca59ab08b7540c8c7fe8019359b027
Jason Heppler <jason.heppler@gmail.com>
2024-01-26 13:58:22 -0600

    Add Censor model to maps view

--- maps/views.py
+++ maps/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
-from postcards.models import Object, Person
+from postcards.models import Censor, Object, Person
 
 
 def maps(request):
     postal_objects = Object.objects.all()
     persons = Person.objects.all()
+    censored_postal_objects = Censor.objects.all()
     mapbox_access_token = "pk.eyJ1IjoiaGVwcGxlcmoiLCJhIjoiY2xwc3cyN3UyMDdlOTJqbTgwcmZjeWJuYSJ9.wmrR3E8vqsQb3Ml7v0HX-A"
     return render(
         request,
@@ -14,5 +15,6 @@ def maps(request):
             "mapbox_access_token": mapbox_access_token,
             "postal_objects": postal_objects,
             "persons": persons,
+            "censored_postal_objects": censored_postal_objects,
         },
     )
