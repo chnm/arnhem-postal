b3cccdd8be6505d2d156c3994518cae8c1c322c1
Jason Heppler <jason.heppler@gmail.com>
2024-02-29 14:02:32 -0600

    Working postmark filtering

--- postcards/filters.py
+++ postcards/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from dateutil.parser import parse
 from django.db.models import Q
 from django.shortcuts import render
 
@@ -89,33 +90,112 @@ class ObjectFilter(django_filters.FilterSet):
         queryset=Location.objects.all(),
         label="Province/State",
     )
-    postmark = django_filters.ModelChoiceFilter(
-        field_name="postmark",
-        queryset=Postmark.objects.all(),
-        to_field_name="location",
-    )
+    postmark = django_filters.CharFilter(method="filter_by_postmark")
     query = django_filters.CharFilter(
         method="filter_query",
         label="Keyword search",
     )
 
-    def filter_query(self, queryset, name, value):
-        """Filter the queryset by the sender or addressee name."""
-        words = value.split(" ")
+    def filter_by_postmark(self, queryset, name, value):
+        if value:
+            # Check if ', dated ' is in the value string
+            if ", dated " in value:
+                # Split the value into location name and date
+                location_name, date = value.split(", dated ", 1)
+            else:
+                # Set location_name to value and date to "unknown date"
+                location_name, date = value, "unknown date"
+            # Remove the "Postmarked at " prefix from the location name
+            location_name = location_name.replace("Postmarked at ", "")
+            # Check if the location name contains a comma
+            if ", " in location_name:
+                # Split the location name into town_city and country
+                town_city, country = location_name.split(", ", 1)
+            else:
+                # Set town_city to location_name and country to an empty string
+                town_city, country = location_name, ""
+            # Create the query for town_city
+            queries = Q(postmark__location__town_city__icontains=town_city)
+            # If country is not an empty string, add a country query
+            if country:
+                queries &= Q(postmark__location__country__icontains=country)
+            # If the date is not "unknown date", parse it and add a date query
+            if date != "unknown date":
+                date = parse(date).date()
+                queries &= Q(postmark__date=date)
+            # Filter the queryset
+            return queryset.filter(queries)
+        return queryset
 
+    def filter_by_date(self, queryset, value):
+        words = value.split(" ")
         queries = Q()
         for word in words:
-            if name == "postmark":
-                queries |= Q(postmark__postmark_id__icontains=word)
-            elif name == "date":
-                queries |= Q(date_of_correspondence__icontains=word)
-            else:
-                queries |= Q(sender_name__first_name=word)
-                queries |= Q(sender_name__last_name=word)
-                queries |= Q(addressee_name__first_name=word)
-                queries |= Q(addressee_name__last_name=word)
-                queries |= Q(location__town_city=word)
-                queries |= Q(location__province_state=word)
-                queries |= Q(public_notes__icontains=word)
-
+            queries |= Q(date_of_correspondence__icontains=word)
         return queryset.filter(queries)
+
+    def filter_by_name(self, queryset, value):
+        first_name, last_name = value.split(" ", 1)
+        queries = Q(
+            sender_name__first_name=first_name, sender_name__last_name=last_name
+        ) | Q(
+            addressee_name__first_name=first_name, addressee_name__last_name=last_name
+        )
+        return queryset.filter(queries)
+
+    def filter_by_city(self, queryset, value):
+        print(f"Filtering by city: {value}")
+        words = value.split(" ")
+        queries = Q()
+        for word in words:
+            queries |= Q(location__town_city__icontains=word)
+        print(f"Queries: {queries}")
+        print(f"Queryset before filtering: {queryset}")
+        queryset = queryset.filter(queries)
+        print(f"Queryset after filtering: {queryset}")
+        return queryset
+
+    def filter_by_state(self, queryset, value):
+        print(f"Filtering by state: {value}")
+        words = value.split(" ")
+        queries = Q()
+        for word in words:
+            queries |= Q(location__province_state__icontains=word)
+        print(f"Queries: {queries}")
+        print(f"Queryset before filtering: {queryset}")
+        queryset = queryset.filter(queries)
+        print(f"Queryset after filtering: {queryset}")
+        return queryset
+
+    def filter_by_collection(self, queryset, value):
+        words = value.split(" ")
+        queries = Q()
+        for word in words:
+            queries |= Q(collection__name__icontains=word)
+        return queryset.filter(queries)
+
+    def filter_by_public_notes(self, queryset, value):
+        words = value.split(" ")
+        queries = Q()
+        for word in words:
+            queries |= Q(public_notes__icontains=word)
+
+    def filter_query(self, queryset, name, value):
+        if name == "postmark":
+            return self.filter_by_postmark(queryset, name, value)
+        elif name == "date":
+            return self.filter_by_date(queryset, value)
+        elif name in [
+            "correspondence",
+        ]:
+            return self.filter_by_name(queryset, value)
+        elif name in ["town_city"]:
+            return self.filter_by_city(queryset, value)
+        elif name in ["province_state"]:
+            return self.filter_by_state(queryset, value)
+        elif name == "collection":
+            return self.filter_by_collection(queryset, value)
+        elif name == "keywords":
+            return self.filter_by_public_notes(queryset, value)
+        else:
+            return queryset
--- postcards/tables.py
+++ postcards/tables.py
@@ -4,13 +4,35 @@ from postcards.models import Object
 
 
 class ItemHtmxTable(tables.Table):
+    thumbnail = tables.TemplateColumn(
+        template_name="postal/thumbnail.html", orderable=False
+    )
+    sender_name = tables.Column(accessor="sender_name", verbose_name="Sender's name")
+    addressee_name = tables.Column(
+        accessor="addressee_name", verbose_name="Addressee's name"
+    )
+    date_of_correspondence = tables.Column()
+    town_city = tables.Column(
+        accessor="sender_name__location__town_city", verbose_name="City"
+    )
+    province_state = tables.Column(
+        accessor="sender_name__location__province_state", verbose_name="Province"
+    )
+    postmark = tables.ManyToManyColumn(
+        separator=", ", accessor="postmark", verbose_name="Postmark"
+    )
+    collection = tables.Column(accessor="collection", verbose_name="Collection")
+
     class Meta:
         model = Object
         template_name = "postal/item_table.html"
         fields = (
+            "thumbnail",
             "sender_name",
             "addressee_name",
             "date_of_correspondence",
+            "town_city",
+            "province_state",
+            "postmark",
             "collection",
-            "view",
         )
