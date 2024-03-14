853bb902bcb4e881c9294cb9ebbd2b9d8600f24b
nguyenxx <nguyenxx@users.noreply.github.com>
2024-03-14 14:48:53 -0400

    update view and path

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from .views import AboutPageView
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
+    path("evolution-of-holocaust/", AboutPageView.as_view(), name="evolution-of-holocaust"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 
 class AboutPageView(TemplateView):
     template_name = "postal/about.html"
+
+class EvolutionOfHolocaustPageView(TemplateView):
+    template_name = "postal/evolution-of-holocaust.html"
