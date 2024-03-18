11bbd880dc5bcb7a05df24d7930ca8f83470e681
nguyenxx <nguyenxx@users.noreply.github.com>
2024-03-18 05:15:49 -0400

    update boa 4

--- postcards/urls.py
+++ postcards/urls.py
@@ -70,6 +70,11 @@ urlpatterns = [
         views.battle_of_arnhem_3,
         name="battle_of_arnhem_3",
     ),
+    path(
+        "exhibits/boa-4/",
+        views.battle_of_arnhem_4,
+        name="battle_of_arnhem_4",
+    ),
     path("map/", views.mapinterface, name="map"),
     path("timeline/", views.timeline, name="timeline"),
     path("resources/", views.resources, name="resources"),
--- postcards/views.py
+++ postcards/views.py
@@ -128,6 +128,11 @@ def battle_of_arnhem_3(request: HttpRequest):
     ctx = {"nav_links": nav_links}
     return render(request, "postal/battle_of_arnhem_3.html", ctx)
 
+def battle_of_arnhem_4(request: HttpRequest):
+    nav_links = get_nav_links("exhibits")
+    ctx = {"nav_links": nav_links}
+    return render(request, "postal/battle_of_arnhem_4.html", ctx)
+
 def timeline(request: HttpRequest):
     nav_links = get_nav_links("timeline")
     ctx = {"nav_links": nav_links}
