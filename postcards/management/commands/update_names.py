d99bbf225eba206afb37f16eaa7e56ccd50c3da9
Jason Heppler <jason.heppler@gmail.com>
2024-05-13 12:29:31 -0500

    fix: update title or entity_name if value is None

--- postcards/management/commands/update_names.py
+++ postcards/management/commands/update_names.py
@@ -1,14 +1,16 @@
 from django.core.management.base import BaseCommand
 from django.db.models import Q
 
-from postcards.models import Object, Person
+from postcards.models import Person
 
 
 class Command(BaseCommand):
     help = "Update the names in the Person model from the Object model."
 
     def handle(slef, *args, **options):
-        people = Person.objects.filter(Q(first_name="None") | Q(last_name="None"))
+        people = Person.objects.filter(
+            Q(first_name="None") | Q(last_name="None") | Q(title="None")
+        )
 
         for person in people:
             if person.first_name == "None":
@@ -17,5 +19,9 @@ class Command(BaseCommand):
             if person.last_name == "None":
                 person.last_name_unknown = True
                 person.last_name = ""
+            if person.title == "None":
+                person.title = ""
+            if person.entity_name == "None":
+                person.entity_name = ""
             person.save()
         print("Names updated")
